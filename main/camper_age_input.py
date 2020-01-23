d89e6ef2dd7e80106db496876abf440a45645377
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-01-22 21:29:53 -0600

    First commit

---
+++ main/camper_age_input.py
@@ -0,0 +1,10 @@
+"""
+Program: camper_age_input.py
+Author: Zachary Hayes
+Last date modified: 01/22/20
+
+Converts camper age from years to months.
+"""
+
+if __name__ == '__main__':
+    print('test')
---
+++ test/test_functions.py
@@ -0,0 +1,15 @@
+import unittest
+from main import camper_age_input
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+def convert_to_months(x):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
